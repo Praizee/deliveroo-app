e3ffc344c159ae356a3afdce74ae6d1921ccdbbe
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-09 20:59:59 +0100

    components

---
+++ components/Categories.js
@@ -0,0 +1,29 @@
+import { ScrollView, View, Text } from "react-native";
+import React from "react";
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+  return (
+    <ScrollView
+      contentContainerStyle={{ paddingHorizontal: 15, paddingTop: 10 }}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    >
+      {/* Category card */}
+
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing1" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing2" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing3" />
+    </ScrollView>
+  );
+};
+
+export default Categories;
