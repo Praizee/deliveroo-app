5c3a2f98d6b2a3f1e28815aed52153e82de84a75
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-10 01:23:11 +0100

    HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,115 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from "react-native-heroicons/outline";
+import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
+// import sanityClient from "../sanity";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  const [featuredCategories, setFeaturedCategories] = useState([]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+      // headerTitle: "Testing",
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   sanityClient
+  //     .fetch(
+  //       `
+  //   *[_type == "featured"] {
+  //     ...,
+  //     restaurants[]->{
+  //       ...,
+  //       dishes[]->
+  //     }
+  //   }    `
+  //     )
+  //     .then((data) => {
+  //       setFeaturedCategories(data);
+  //     });
+  // }, []);
+
   return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-6">
+      {/* <Text className="text-red-500">HomeScreen</Text> */}
+
+      {/* header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and Cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      {/* body */}
+      <ScrollView
+        className="bg-gray-100 border-2"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {/* Categories */}
+        <Categories />
+
+        {/* Featured */}
+        <FeaturedRow
+          id="1"
+          title="Featured"
+          description="Paid placements from out partners"
+        />
+
+        {/* Tasty Discounts */}
+        <FeaturedRow
+          id="12"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts"
+        />
+
+        {/* Offers near you */}
+        <FeaturedRow
+          id="123"
+          title="Offers near you!"
+          description="Why not support your local restaurant tonight!"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
